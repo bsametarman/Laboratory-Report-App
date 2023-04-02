089cea514529428ae27c29fefc8f077532ce8869
bsametarman@outlook.com <bsametarman@outlook.com>
2023-04-02 11:53:23 +0300

    first commit

---
+++ src/main/java/com/project/LaboratoryReportApp/LaboratoryReportAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.LaboratoryReportApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryReportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryReportAppApplication.class, args);
+	}
+
+}
