06c3587c463a6295ed034e61983bedbc4789a16e
bsametarman@outlook.com <bsametarman@outlook.com>
2023-04-04 12:42:48 +0300

    model mapper

--- src/main/java/com/project/LaboratoryReportApp/LaboratoryReportAppApplication.java
+++ src/main/java/com/project/LaboratoryReportApp/LaboratoryReportAppApplication.java
@@ -1,13 +1,20 @@
 package com.project.LaboratoryReportApp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LaboratoryReportAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaboratoryReportAppApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 	
 }
